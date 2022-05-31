b11ca2a93a5e3d1c1e3803a0f6e867f6a32657d8
Kamran Haq <KamranHaq@protonmail.com>
2022-05-30 17:51:51 -0700

    Implement Basic p2p Functions

--- Assets/Lobby.cs
+++ Assets/Lobby.cs
@@ -15,18 +15,28 @@ public class Lobby : MonoBehaviour
     const int MAX_MEMBERS = 8;
     private CSteamID m_Lobby = new CSteamID(0); //Current lobby ID
     private CSteamID currentUserID;
+    private CSteamID hostID = new CSteamID(0);
     private string sessionKey = "EKRX82Z";
     private string gameID = "CFPS";
-
+    private bool localIsHost = false;
+    private bool connected = false;
+    const int READLIMIT = 999;
 
     enum MsgType : uint
     {
-        Ping,
-        Ack, //Acknowledge
+        PING, //Ping recipient
+        ACK, //Acknowledge ping
+        HGDU, //Game data update from host
+        GDUH, //Game data update to host
     }
 
     //Callback Declarations
     protected Callback<LobbyDataUpdate_t> m_LobbyDataUpdate;
+    protected Callback<LobbyChatUpdate_t> m_LobbyChatUpdate;
+    protected Callback<P2PSessionRequest_t> m_P2PSessionRequest;
+    protected Callback<P2PSessionConnectFail_t> m_P2PSessionConnectFail;
+    protected Callback<SocketStatusCallback_t> m_SocketStatusCallback;
+
 
 
     //Callresult Declarations
@@ -42,6 +52,10 @@ public class Lobby : MonoBehaviour
 
             //Callbacks
             m_LobbyDataUpdate = Callback<LobbyDataUpdate_t>.Create(OnLobbyDataUpdate);
+            m_LobbyChatUpdate = Callback<LobbyChatUpdate_t>.Create(OnLobbyChatUpdate);
+            m_P2PSessionRequest = Callback<P2PSessionRequest_t>.Create(OnP2PSessionRequest);
+            m_P2PSessionConnectFail = Callback<P2PSessionConnectFail_t>.Create(OnP2PSessionConnectFail);
+            m_SocketStatusCallback = Callback<SocketStatusCallback_t>.Create(OnSocketStatusCallback);
 
             //CallResults
             m_OnLobbyCreatedCallResult = CallResult<LobbyCreated_t>.Create(OnLobbyCreated);
@@ -59,9 +73,9 @@ public class Lobby : MonoBehaviour
             Debug.Log("[STEAM] Initialized");
             Debug.Log("[STEAM] Current Username: " + SteamFriends.GetPersonaName());
             members.Add(currentUserID);
-            CreateLobby();
         }
-        else {
+        else 
+        {
             Debug.LogError("[STEAM] Not Initialized");
             return;
         }
@@ -70,8 +84,90 @@ public class Lobby : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (SteamManager.Initialized && m_Lobby.m_SteamID > 0) { 
-            //Send and receive packets
+        if (SteamManager.Initialized && m_Lobby.m_SteamID > 0) 
+        {
+            if (localIsHost)
+            {
+                foreach (CSteamID member in members) //Send data to all connected users if host
+                {
+                    if (member != currentUserID)
+                    {
+                        //SendPacket(member, new byte[0])
+                    }
+                }
+
+            }
+            else
+            {
+                //Send the host local user data
+                //SendPacket(hostID, new byte[0]);
+            }
+            ReadAllPackets();
+        }
+    }
+
+    private void SendPing(CSteamID recipient) 
+    {
+        byte[] bytes = new byte[4];
+        using (System.IO.MemoryStream ms = new System.IO.MemoryStream(bytes))
+        using (System.IO.BinaryWriter b = new System.IO.BinaryWriter(ms))
+        {
+            b.Write((uint)MsgType.PING);
+        }
+        bool ret = SteamNetworking.SendP2PPacket(recipient, bytes, (uint)bytes.Length, EP2PSend.k_EP2PSendReliable);
+    }
+
+    private void SendAck(CSteamID recipient) 
+    {
+        byte[] bytes = new byte[4];
+        using (System.IO.MemoryStream ms = new System.IO.MemoryStream(bytes))
+        using (System.IO.BinaryWriter b = new System.IO.BinaryWriter(ms))
+        {
+            b.Write((uint)MsgType.ACK);
+        }
+        bool ret = SteamNetworking.SendP2PPacket(recipient, bytes, (uint)bytes.Length, EP2PSend.k_EP2PSendReliable);
+    }
+
+    private void SendPacket(CSteamID recipient, byte[] data) 
+    {
+        EP2PSend sendType = EP2PSend.k_EP2PSendReliable;
+        bool ret = SteamNetworking.SendP2PPacket(recipient, data, (uint)data.Length, sendType);
+    }
+
+    private void ReadAllPackets(int readCount = 0) {
+        uint MsgSize;
+        if (SteamNetworking.IsP2PPacketAvailable(out MsgSize) && readCount < READLIMIT) {
+            ReadPacket(MsgSize);
+            ReadAllPackets(readCount + 1);
+        }
+    }
+
+    private void ReadPacket(uint MsgSize) 
+    {
+        byte[] bytes = new byte[MsgSize];
+        uint newMsgSize;
+        CSteamID SteamIdRemote;
+        bool ret = SteamNetworking.ReadP2PPacket(bytes, MsgSize, out newMsgSize, out SteamIdRemote);
+
+        using (System.IO.MemoryStream ms = new System.IO.MemoryStream(bytes))
+        using (System.IO.BinaryReader b = new System.IO.BinaryReader(ms))
+        {
+            MsgType msgtype = (MsgType)b.ReadUInt32();
+            switch (msgtype) 
+            {
+                case MsgType.PING:
+                    Debug.Log("[STEAM] Pinged by peer " + SteamIdRemote.m_SteamID);
+                    SendAck(SteamIdRemote);
+                    break;
+                case MsgType.ACK:
+                    Debug.Log("[STEAM] Acknowledged by host " + SteamIdRemote.m_SteamID);
+                    break;
+                case MsgType.HGDU:
+                    break;
+                case MsgType.GDUH:
+                    break;
+            }
+            print("SteamNetworking.ReadP2PPacket(bytes, " + MsgSize + ", out newMsgSize, out SteamIdRemote) - " + ret + " -- " + newMsgSize + " -- " + SteamIdRemote + " -- " + msgtype);
         }
     }
 
@@ -82,7 +178,8 @@ public class Lobby : MonoBehaviour
             SteamAPICall_t handle = SteamMatchmaking.CreateLobby(ELobbyType.k_ELobbyTypePublic, MAX_MEMBERS);
             m_OnLobbyCreatedCallResult.Set(handle);
             Debug.Log("[STEAM] Called: CreateLobby()");
-           
+            localIsHost = true;
+            hostID = currentUserID;
         }
     }
 
@@ -103,17 +200,57 @@ public class Lobby : MonoBehaviour
         Debug.Log("[STEAM] Updating lobby member list for " + m_Lobby.m_SteamID);
         members.Clear();
         members.Add(currentUserID);
-        if (m_Lobby.m_SteamID > 0) { 
+        if (m_Lobby.m_SteamID > 0) 
+        { 
             int numMembers = SteamMatchmaking.GetNumLobbyMembers(m_Lobby);
-            for (int i = 0; i < numMembers; i++) {
+            for (int i = 0; i < numMembers; i++) 
+            {
                 CSteamID member = SteamMatchmaking.GetLobbyMemberByIndex(m_Lobby, i);
                 Debug.Log(SteamFriends.GetFriendPersonaName(member));
-                if (member != currentUserID) {
+                if (member != currentUserID) 
+                {
                     members.Add(member);
                 }
             }
+
+            if (!localIsHost)
+            {
+                FindHost();
+                SendPing(hostID);
+                Debug.Log("[STEAM] Pinging Host");
+            }
         }
-    
+    }
+
+    private void LeaveLobby() {
+        SteamMatchmaking.LeaveLobby(m_Lobby);
+        m_Lobby = new CSteamID(0);
+        if (localIsHost) {
+            hostID = new CSteamID(0);
+        }
+
+        localIsHost = false;
+        connected = false;
+    }
+
+    private void FindHost()
+    {
+        foreach (CSteamID member in members)
+        {
+            if (member != currentUserID)
+            {
+                string hostStatus = SteamMatchmaking.GetLobbyMemberData(m_Lobby, member, "isHost");
+                if (hostStatus != null)
+                {
+                    if (hostStatus == "true")
+                    {
+                        hostID = member;
+                        Debug.Log("[STEAM] Current Host: " + member.m_SteamID);
+                        break;
+                    }
+                }
+            }
+        }
     }
 
     /*
@@ -133,7 +270,10 @@ public class Lobby : MonoBehaviour
         m_Lobby = (CSteamID)pCallback.m_ulSteamIDLobby;
         SteamMatchmaking.SetLobbyData(m_Lobby, "sessionKey", sessionKey);
         SteamMatchmaking.SetLobbyData(m_Lobby, "game", gameID);
+        SteamMatchmaking.SetLobbyMemberLimit(m_Lobby, MAX_MEMBERS);
+        connected = true;
         UpdateLobbyMembers();
+        SteamMatchmaking.SetLobbyMemberData(m_Lobby, "isHost", "true");
     }
 
     /*
@@ -157,9 +297,12 @@ public class Lobby : MonoBehaviour
      */
     private void OnLobbyEntered(LobbyEnter_t pCallback, bool bIOFailure) 
     {
+        SteamMatchmaking.SetLobbyMemberData(m_Lobby, "isHost", "false");
         Debug.Log("[STEAM] Lobby Entered: " + pCallback.m_ulSteamIDLobby);
         m_Lobby = new CSteamID(pCallback.m_ulSteamIDLobby);
+        connected = true;
         UpdateLobbyMembers();
+        
     }
 
     /*
@@ -171,5 +314,41 @@ public class Lobby : MonoBehaviour
         UpdateLobbyMembers();
     }
 
+    /*
+     * Callback
+     * Called when player leaves
+     */
+    private void OnLobbyChatUpdate(LobbyChatUpdate_t pCallback)
+    {
+        Debug.Log("[STEAM] Chat update -- Player left? -- for " + m_Lobby.m_SteamID);
+        UpdateLobbyMembers();
+    }
+
+    /*
+     * Callback
+     */
+    void OnP2PSessionRequest(P2PSessionRequest_t pCallback)
+    {
+        Debug.Log("[" + P2PSessionRequest_t.k_iCallback + " - P2PSessionRequest] - " + pCallback.m_steamIDRemote);
+
+        bool ret = SteamNetworking.AcceptP2PSessionWithUser(pCallback.m_steamIDRemote);
+        print("SteamNetworking.AcceptP2PSessionWithUser(" + pCallback.m_steamIDRemote + ") - " + ret);
+    }
+
+    /*
+     * Callback
+     */
+    void OnP2PSessionConnectFail(P2PSessionConnectFail_t pCallback)
+    {
+        Debug.Log("[" + P2PSessionConnectFail_t.k_iCallback + " - P2PSessionConnectFail] - " + pCallback.m_steamIDRemote + " -- " + pCallback.m_eP2PSessionError);
+    }
+
+    /*
+     * Callback
+     */
+    void OnSocketStatusCallback(SocketStatusCallback_t pCallback)
+    {
+        Debug.Log("[" + SocketStatusCallback_t.k_iCallback + " - SocketStatusCallback] - " + pCallback.m_hSocket + " -- " + pCallback.m_hListenSocket + " -- " + pCallback.m_steamIDRemote + " -- " + pCallback.m_eSNetSocketState);
+    }
 
 }
